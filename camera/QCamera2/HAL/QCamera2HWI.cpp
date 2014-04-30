02e250edd8a58e33b00973aafd1838dd7916e65d
Emilian Peev <epeevs@codeaurora.org>
2014-04-30 15:13:41 -0700

    ham:QCamera2: Don't initialize preview channel in ready state
     Flash torch configuration shouldn't trigger
     preview channel initialization/release once
     we are in preview ready state. At this point
     the client has already called 'startPreview'
     and the flash configuration will get applied
     once a valid preview window starts preview.
    
    Change-Id: Ib44af3af593daabc7f1b901cd0b74eee773d20ad
    Signed-off-by: sriramprakhya <sriramscience7@gmail.com>

--- camera/QCamera2/HAL/QCamera2HWI.cpp
+++ camera/QCamera2/HAL/QCamera2HWI.cpp
@@ -1376,7 +1376,8 @@ int QCamera2HardwareInterface::prepareTorchCamera()
     int rc = NO_ERROR;
 
     if ( ( !m_stateMachine.isPreviewRunning() ) &&
-         ( m_channels[QCAMERA_CH_TYPE_PREVIEW] == NULL ) ) {
+            !m_stateMachine.isPreviewReady() &&
+            ( m_channels[QCAMERA_CH_TYPE_PREVIEW] == NULL ) ) {
         rc = addChannel(QCAMERA_CH_TYPE_PREVIEW);
     }
 
@@ -1398,7 +1399,8 @@ int QCamera2HardwareInterface::prepareTorchCamera()
 int QCamera2HardwareInterface::releaseTorchCamera()
 {
     if ( !m_stateMachine.isPreviewRunning() &&
-         ( m_channels[QCAMERA_CH_TYPE_PREVIEW] != NULL ) ) {
+            !m_stateMachine.isPreviewReady() &&
+            ( m_channels[QCAMERA_CH_TYPE_PREVIEW] != NULL ) ) {
         delete m_channels[QCAMERA_CH_TYPE_PREVIEW];
         m_channels[QCAMERA_CH_TYPE_PREVIEW] = NULL;
     }
