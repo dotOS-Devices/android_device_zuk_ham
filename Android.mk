9b50f719845397d43900c12b4a42d368c6251b0b
Bruno Martins <bgcngm@gmail.com>
2017-09-04 00:40:11 +0100

    ham: Move WiFi files to vendor
     * The config is now expected to be placed there.
    
     * Move the rest of it to be coherent.
    
     * Edit: Fix "/" typo
    
    Change-Id: Ib6fdbb23df25c8ebfebb7b5c58005723b3348892
    Signed-off-by: OrdenKrieger <OrdenKriegerEDR@googlemail.com>
    Signed-off-by: sriramprakhya <sriramscience7@gmail.com>

--- Android.mk
+++ Android.mk
@@ -19,11 +19,13 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),ham)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+WCNSS_CONFIG_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_cfg.ini
+$(WCNSS_CONFIG_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "WCNSS config link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /data/misc/wifi/$(notdir $@) $@
 
-# Create a link for the WCNSS config file, which ends up as a writable
-# version in /data/misc/wifi
-$(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/prima; \
-     ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
-     	    $(TARGET_OUT)/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
+ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_CONFIG_SYMLINK)
 
 endif
