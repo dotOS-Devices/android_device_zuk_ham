ef0f8734c268d282562776ba30a89e74866590d7
Ethan Chen <intervigil@gmail.com>
2017-09-14 01:01:28 -0700

    ham: Add seccomp policy
    Change-Id: I1d5017228a4a9318f8a77ef4036532d64718ecbb
    Signed-off-by: Srijith <srijithdamerla2001@gmail.com>

--- ham.mk
+++ ham.mk
@@ -209,6 +209,7 @@ PRODUCT_PACKAGES += \
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
+    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
