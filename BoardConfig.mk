a046ddcc03238a631ea7d73b72263f98639482c5
leodiram <sriramscience7@gmail.com>
2018-03-26 09:39:14 +0000

    BC.mk:Fix cherry-pick errors

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -203,16 +203,8 @@ TARGET_HW_DISK_ENCRYPTION := true
 # Added to indicate that protobuf-c is supported in this build
 PROTOBUF_SUPPORTED := true
 
-<<<<<<< HEAD
 # ANT+ - TODO: Confirm this - TODO: Confirm this - TODO: Confirm this - TODO: Confirm this
-#BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
-=======
-# Render
-OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
-SF_START_GRAPHICS_ALLOCATOR_SERVICE := true
-USE_OPENGL_RENDERER := true
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
->>>>>>> bada10a... shamu: Enable SF_START_GRAPHICS_ALLOCATOR_SERVICE
+BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
 
 #Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
