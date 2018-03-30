617b3fef6c092610d883b9af4c6aab565f222fdd
leodiram <sriramscience7@gmail.com>
2018-03-30 04:45:45 +0000

    ham: treble fixes

--- treble.mk
+++ treble.mk
@@ -26,11 +26,10 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-impl-legacy \
     android.hardware.camera.provider@2.4-service.ham \
-    android.hardware.camera.provider@2.4-service \
-    camera.device@1.0-impl\
-    camera.device@3.2-impl     
+    camera.device@1.0-impl-legacy \
+    camera.device@3.2-impl-legacy
 
 # DRM
 PRODUCT_PACKAGES += \
