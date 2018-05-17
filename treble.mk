8fb8a484804b5ef985c912924f1a99cc730a3747
Srijith <srijithdamerla2001@gmail.com>
2018-05-17 13:01:15 +0000

    ham: Build sound trigger

--- treble.mk
+++ treble.mk
@@ -22,7 +22,8 @@ PRODUCT_PACKAGES += \
 # audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.soundtrigger@2.0-impl
 
 # Camera
 PRODUCT_PACKAGES += \
